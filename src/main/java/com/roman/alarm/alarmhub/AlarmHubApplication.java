048d1c11ebc7ce1b171fb33a1d0fb00cc1dcd394
zhaoxiang <zhaoxiang@tianyancha.com>
2023-06-12 11:28:24 +0800

    Initial commit

---
+++ src/main/java/com/roman/alarm/alarmhub/AlarmHubApplication.java
@@ -0,0 +1,13 @@
+package com.roman.alarm.alarmhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/alarm/alarmhub/AlarmHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.alarm.alarmhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
